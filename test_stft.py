cc5af9afb93dd1f7fcd26ad0369d38022f5c471e
adiyoss <yossiadidrum@gmail.com>
2019-01-03 14:16:09 +0200

    add plot

--- test_stft.py
+++ test_stft.py
@@ -16,20 +16,38 @@ def test_stft():
     def to_np(tensor):
         return tensor.cpu().data.numpy()
 
-    for i in range(12):
-        filter_length = 2**i
-        for j in range(i+1):
-            try:
-                hop_length = 2**j
-                stft = STFT(filter_length=filter_length, hop_length=hop_length)
-                if torch.cuda.is_available():
-                    stft = stft.cuda()
-                output, m_hat, p_hat = stft(audio)
-                loss = mse(audio, output)
-                print('Audio MSE: %s @ filter_length = %d, hop_length = %d' % (str(to_np(loss).item()), filter_length, hop_length))
-            except:
-                print('Failed @ filter_length = %d, hop_length = %d' % (filter_length, hop_length))
-                print(traceback.print_exception())
+    filter_length=320
+    hop_length=160
+    stft = STFT(filter_length=filter_length, hop_length=hop_length)
+    if torch.cuda.is_available():
+        stft = stft.cuda()
+    output, m_hat, p_hat = stft(audio)
+
+    D = librosa.stft(y, n_fft=filter_length, hop_length=hop_length)
+    m, p = librosa.magphase(D)
+
+    import ipdb; ipdb.set_trace()
+
+    import matplotlib.pyplot as plt
+    librosa.display.specshow(librosa.amplitude_to_db(D, ref=np.max), y_axis='log', x_axis='time')
+    plt.title('Power spectrogram')
+    plt.colorbar(format='%+2.0f dB')
+    plt.tight_layout()
+
+    #for i in range(12):
+    #    filter_length = 2**i
+    #    for j in range(i+1):
+    #        try:
+    #            hop_length = 2**j
+    #            stft = STFT(filter_length=filter_length, hop_length=hop_length)
+    #            if torch.cuda.is_available():
+    #                stft = stft.cuda()
+    #            output, m_hat, p_hat = stft(audio)
+    #            loss = mse(audio, output)
+    #            print('Audio MSE: %s @ filter_length = %d, hop_length = %d' % (str(to_np(loss).item()), filter_length, hop_length))
+    #        except:
+    #            print('Failed @ filter_length = %d, hop_length = %d' % (filter_length, hop_length))
+    #            print(traceback.print_exception())
 
 if __name__ == "__main__":
     test_stft()
